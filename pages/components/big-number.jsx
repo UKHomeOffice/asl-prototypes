0c5d037e7c7f907434f84fdfb7f49b563e0e8cce
Leonard Martin <leonard.martin@gmail.com>
2018-04-12 12:52:42 +0100

    Add search box

--- pages/components/big-number.jsx
+++ pages/components/big-number.jsx
@@ -5,7 +5,7 @@ class BigNumber extends React.Component {
   render() {
     return <div className="data">
       <span className="data-item bold-xlarge">{ this.props.number }</span>
-      <span className="data-item bold-xsmall">{ this.props.label }</span>
+      <span className="data-item bold-small">{ this.props.label }</span>
     </div>;
   }
 
--- pages/home.jsx
+++ pages/home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Page, Form, Table } from 'prototype-kit';
+import { some } from 'lodash';
 
 import BigNumber from './components/big-number';
 
@@ -14,30 +15,47 @@ class Home extends Page {
     }
   }
 
+  greet() {
+    return (new Date()).getHours() < 12 ? 'Good morning Ben' : 'Good afternoon Ben';
+  }
+
+  search() {
+    if (!this.state || !this.state.searchTerm) {
+      return this.props.data.establishments;
+    }
+    const term = this.state.searchTerm;
+    return this.props.data.establishments.filter(row => {
+      return some(['name', 'licenceNumber'], key => row[key].toLowerCase().includes(term.toLowerCase()));
+    });
+  }
+
   content() {
+    const filtered = this.search();
     return <React.Fragment>
-      <h1>Good morning Ben</h1>
+      <h1>{ this.greet() }</h1>
       <div className="grid-row">
         <div className="column-one-quarter">
           <BigNumber number={ this.props.data.establishments.length } label="Establishments" />
         </div>
         <div className="column-one-quarter">
-          <BigNumber number="17000" label="Personal licences" />
+          <BigNumber number="17000" label={<a href="/personal-licences">Personal licences</a>} />
         </div>
         <div className="column-one-quarter">
-          <BigNumber number="500" label="Projects" />
+          <BigNumber number="500" label={<a href="/projects">Projects</a>} />
         </div>
         <div className="column-one-quarter">
           <aside>
             <Form.Input
+              className="form-control-3-4"
               label="Search establishments"
               hint="By establishment name or licence number"
+              onChange={e => this.setState({ searchTerm: e.target.value})}
               />
           </aside>
         </div>
       </div>
       <Table
-        dataset={ this.props.data.establishments }
+        dataset={ filtered }
         columns={ { name: 'Establishment name', licenceNumber: 'Licence number' } }
         formatter={ (key, value, row) => this.cell(key, value, row) }
         sort="name"
